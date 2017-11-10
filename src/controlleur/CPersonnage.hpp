d9561aa4231faf5ce2ff2a72874291bb491022b4
FederAndInk <federandink@gmail.com>
2017-11-10 16:44:00 +0100

    [BEGINING] add some classes and files to make skel

---
+++ src/controlleur/CPersonnage.hpp
@@ -0,0 +1,35 @@
+/**
+ * CPersonnage.hpp
+ *
+ * Early optimization is the root of all evil
+ * 
+ *  Created on: 10 nov. 2017
+ *      Author: nihil
+ */
+
+#pragma once
+
+class CPersonnage
+{
+//========================>Attributes<========================
+private:
+
+//=======================>Constructors<=======================
+public:
+  CPersonnage();
+  // TODO: rule of five ? copyandswap
+  virtual ~CPersonnage();
+
+private:
+
+//=========================>Methods<==========================
+public:
+
+private:
+
+//=====================>Getters&Setters<======================
+public:
+
+private:
+
+};
---
+++ src/model/MTuile.hpp
@@ -0,0 +1,35 @@
+/**
+ * MTuile.hpp
+ *
+ * Early optimization is the root of all evil
+ * 
+ *  Created on: 10 nov. 2017
+ *      Author: nihil
+ */
+
+#pragma once
+
+class MTuile
+{
+//========================>Attributes<========================
+private:
+
+//=======================>Constructors<=======================
+public:
+  MTuile();
+  // TODO: rule of five ? copyandswap
+  virtual ~MTuile();
+
+private:
+
+//=========================>Methods<==========================
+public:
+
+private:
+
+//=====================>Getters&Setters<======================
+public:
+
+private:
+
+};
