60e6f56e4491599a7bd8cc4d2b5cdbfab388a288
FederAndInk <federandink@gmail.com>
2018-01-11 17:57:21 +0100

    [FIX] black uglies horizontal rows

--- src/controlleur/CJeu.cpp
+++ src/controlleur/CJeu.cpp
@@ -29,7 +29,11 @@
 //------------------------------------------------------------
 
 CJeu::CJeu() :
-    vuePrincipale(new AppFrame("A Space Story", wxPoint(0, 0), wxSize(136, 100), 32)),
+    vuePrincipale(
+        new AppFrame("A Space Story", wxPoint(0, 0),
+            wxSize(wxSystemSettings::GetMetric(wxSYS_SCREEN_X) - 10,
+                   wxSystemSettings::GetMetric(wxSYS_SCREEN_Y) - 50),
+            32)),
     cNiveau(vuePrincipale), cPersonnage(vuePrincipale, &cNiveau.getTerrain()), cLua(this)
 {
 
